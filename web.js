770747000607276216f14b2f94e00927a12fe50a
dmv1 <donald.voltz@gmail.com>
2013-07-10 16:27:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var file_contents = fs.readFileSync('index.html', 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  //response.send('Hello World 2!');
+  response.send(file_contents);
+}); 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
